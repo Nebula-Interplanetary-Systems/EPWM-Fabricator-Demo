d5956dbd96fc32cb3f388b7290ba698b435f274b
troy-nebula <109788103+troy-nebula@users.noreply.github.com>
2024-03-20 10:58:09 +1100

    Trying to explore why each module is sharing the same duty as the difference between the two desired duty cycles  for each channel

--- epwm_demo.c
+++ epwm_demo.c
@@ -53,12 +53,13 @@ uint32_t gEpwmBaseAddr[3];
 
 void epwm_duty_cycle_main(void *args)
 {
-    bool                increasing[2][2] = {{true, true}, {true, true}};
     ePWM_PARAMS         epwm_config[3];
-    uint32_t            dutyCycle[2][2] = {{0, 25}, {50, 75}};
+    bool                increasing[3][2] = {{true, true}, {true, true}, {true, true}};
+    uint32_t            dutyCycle[3][2] = {{0, 20}, {40, 60}, {80, 90}};
 
     updateEpwmOutputFreq(&epwm_config[0], 1U * 1000U);
-    updateEpwmOutputFreq(&epwm_config[1], 1U * 1000U);
+    updateEpwmOutputFreq(&epwm_config[1], 2U * 1000U);
+    updateEpwmOutputFreq(&epwm_config[2], 3U * 1000U);
 
     /* Open drivers to open the UART driver for console */
     Drivers_open();
@@ -67,26 +68,41 @@ void epwm_duty_cycle_main(void *args)
     /* Address translate */
     gEpwmBaseAddr[0] = (uint32_t)AddrTranslateP_getLocalAddr(CONFIG_EPWM0_BASE_ADDR);
     gEpwmBaseAddr[1] = (uint32_t)AddrTranslateP_getLocalAddr(CONFIG_EPWM1_BASE_ADDR);
+    gEpwmBaseAddr[2] = (uint32_t)AddrTranslateP_getLocalAddr(CONFIG_EPWM2_BASE_ADDR);
+
+    /* Set the FSI_EPWM2 MUX SEL pin */
+    GPIO_setDirMode(EPWM2_MUX_BASE_ADDR, EPWM2_MUX_PIN, GPIO_DIRECTION_OUTPUT);
+    GPIO_pinWriteHigh(EPWM2_MUX_BASE_ADDR, EPWM2_MUX_PIN);
+    if(GPIO_pinOutValueRead(EPWM2_MUX_BASE_ADDR, EPWM2_MUX_PIN) != GPIO_PIN_HIGH) { 
+        DebugP_log("[ePWM] MUX select pin did not set. ePWM2 not selected.\r\n");
+    } else {
+        DebugP_log("[ePWM] MUX select pin set to HIGH. ePWM2 selected.\r\n");
+    }
 
     DebugP_log("EPWM Duty Cycle Sweep Started ...\r\n");
 
-    while(1U) {
-        for (uint8_t i = 0; i < 2; i++) {
-            for (uint8_t j = 0; j < 2; j++) {
+    //while(1U) {
+        for (int8_t i = 2; i >= 0; i--) {
+            for (int8_t j = 1; j >= 0; j--) {
                 // Update duty cycle value based on direction
-                increasing[i][j] ? ++dutyCycle[i][j] : --dutyCycle[i][j];
+                /*increasing[i][j] ? ++dutyCycle[i][j] : --dutyCycle[i][j];
                 increasing[i][j] = (dutyCycle[i][j] >= 100) ? false : (dutyCycle[i][j] <= 0 ? true : increasing[i][j]);
+                dutyCycle[i][j] > 100 ? (dutyCycle[i][j] = 100) : (dutyCycle[i][j] < 0 ? (dutyCycle[i][j] = 0) : dutyCycle[i][j]);
+            */
+                DebugP_log("EPWM%d_%c: %d%%\r\n", i, (j == 0 ? 'A' : 'B'), dutyCycle[i][j]);
 
                 /* Update the ePWM params with the new duty cycle */
-                updateEpwmDutyCycle(&epwm_config[i], dutyCycle[i][j], (j == 0 ? EPWM_CC_CMP_A : EPWM_CC_CMP_B));
+                updateEpwmDutyCycle(&epwm_config[i], dutyCycle[i][j], (j == 0 ? EPWM_OUTPUT_CH_A : EPWM_OUTPUT_CH_B));
 
                 /* Configure PWM for the new duty cycle */
                 App_epwmConfig(gEpwmBaseAddr[i], (j == 0 ? EPWM_OUTPUT_CH_A : EPWM_OUTPUT_CH_B), CONFIG_EPWM0_FCLK, &epwm_config[i]);
+                
+                /* The sweep should happen over 1 second */
+                vTaskDelay(pdMS_TO_TICKS(1000U));
             }
         }
-        /* The sweep should happen over 1 second */
-        vTaskDelay(pdMS_TO_TICKS(10U));
-    }
+
+    //}
 
     Board_driversClose();
     Drivers_close();
@@ -108,12 +124,8 @@ static void App_epwmConfig(uint32_t     epwmBaseAddr,
     EPWM_tbSetEmulationMode(epwmBaseAddr, EPWM_TB_EMU_MODE_FREE_RUN);
 
     /* Configure counter compare submodule */
-    EPWM_counterComparatorCfg(epwmBaseAddr, EPWM_CC_CMP_A,
-        epwm_config->compVal[0], EPWM_SHADOW_REG_CTRL_ENABLE,
-        EPWM_CC_CMP_LOAD_MODE_CNT_EQ_ZERO, TRUE);
-    EPWM_counterComparatorCfg(epwmBaseAddr, EPWM_CC_CMP_B,
-        epwm_config->compVal[1], EPWM_SHADOW_REG_CTRL_ENABLE,
-        EPWM_CC_CMP_LOAD_MODE_CNT_EQ_ZERO, TRUE);
+    EPWM_counterComparatorCfg(epwmBaseAddr, EPWM_CC_CMP_A, epwm_config->compVal[0], EPWM_SHADOW_REG_CTRL_ENABLE, EPWM_CC_CMP_LOAD_MODE_CNT_EQ_ZERO, TRUE);
+    EPWM_counterComparatorCfg(epwmBaseAddr, EPWM_CC_CMP_B, epwm_config->compVal[1], EPWM_SHADOW_REG_CTRL_ENABLE, EPWM_CC_CMP_LOAD_MODE_CNT_EQ_ZERO, TRUE);
 
     /* Configure Action Qualifier Submodule */
     aqConfig.zeroAction = EPWM_AQ_ACTION_DONOTHING;
